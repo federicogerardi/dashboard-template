6e67f7c1ee052cb2326ddb67d6760ee99de95420
federicogerardi <federico.gerardi@gmail.com>
2025-01-03 15:15:44 +0100

    rimosso plugin di prova

--- app/plugins/prova_nuovo/models.py
+++
@@ -1,3 +0,0 @@
-from app.extensions import db
-
-# Definisci qui i tuoi modelli
--- app/plugins/prova_nuovo/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from flask_login import login_required
-
-@login_required
-def index():
-    return render_template('prova_nuovo/index.html')
